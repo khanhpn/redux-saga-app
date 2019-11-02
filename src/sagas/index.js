9831e1051ec3e98027a0d665dcfc352a8ca7c03b
khanhpn <khanhpham@bravesoft.vn>
2019-11-02 22:34:36 +0700

    using redux saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,6 @@
-import { fork, take, call, put } from 'redux-saga/effects';
+import { fork, take, call, put, delay } from 'redux-saga/effects';
 import * as taskTypes from '../constants/task';
+import { showLoading, hideLoading } from '../actions/ui';
 import { getList } from '../apis/task';
 import { STATUS_CODE } from '../constants';
 import { fetchListTaskSuccess, fetchListTaskFailed } from '../actions/task';
@@ -7,6 +8,7 @@ import { fetchListTaskSuccess, fetchListTaskFailed } from '../actions/task';
 function* watchFetchListTaskAction() {
   while (true) {
     yield take(taskTypes.FETCH_TASK);
+    yield put(showLoading());
     const response = yield call(getList);
     const { status, data } = response;
     if (status === STATUS_CODE.SUCCESS) {
@@ -14,6 +16,8 @@ function* watchFetchListTaskAction() {
     } else {
       yield put(fetchListTaskFailed(data));
     }
+    yield delay(1000);
+    yield put(hideLoading());
   }
 }
 
